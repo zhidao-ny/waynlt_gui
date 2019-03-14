35eb36642b8f6ef2f65e490cb0b66c31d7cd686e
Jizheng Yang <jy1700@nyu.edu>
2019-03-13 20:38:47 -0400

    fix button

--- yuhan_ZD+Project.py
+++ yuhan_ZD+Project.py
@@ -20,7 +20,8 @@ ZD = tk.Tk()
 
 #add a title
 ZD.title("ZHIDAO CAREER SEARCH")
-
+ZD.geometry("1200x700")
+ZD.resizable(0,0)
 #fig = plt.figure()
 
 # Add logo
@@ -28,7 +29,7 @@ image = Image.open("zhidaopic1.gif")
 photo = ImageTk.PhotoImage(image, master = ZD)
 label = tk.Label(ZD, image=photo)
 label.image = image
-label.place(x=50,y=30,relx = 0.45, rely = 0.15 ,anchor = CENTER)
+label.place(x=50,y=30,relx = 0.45, rely = 0.15, anchor = tk.CENTER)
 
 
 
@@ -69,22 +70,24 @@ def click_me2():
     action.configure(text = "Search" + name.get() + '' + number_chosen.get())
     
 #Changing our Label 2
-ttk.Label(ZD, text = "Enter Your Search1:").pack(side = 'left', anchor = CENTER)#grid(column = 0, row = 3)
+#ttk.Label(ZD, text = "Enter Your Search1:").pack(side = 'left', anchor = tk.CENTER)#grid(column = 0, row = 3)
 
 # Adding a Textbox Entry widget
-name = tk.StringVar()
-name_entered = ttk.Entry(ZD, width = 12, textvariable = name).pack(side = 'left', anchor = CENTER)#grid(column = 0, row = 4)
+#name = tk.StringVar()
+#name_entered = ttk.Entry(ZD, width = 12, textvariable = name).pack(side = 'left', anchor = tk.CENTER)#grid(column = 0, row = 4)
 
-ttk.Label(ZD, text = "Choose an skillset:").pack(side = 'left', anchor = CENTER)#grid(column = 1, row = 3)
+ttk.Label(ZD, text = "Choose an skillset:").pack(side = 'left', anchor = tk.CENTER)#grid(column = 1, row = 3)
 number = tk.StringVar()
-number_chosen = ttk.Combobox (ZD, width = 12, textvariable = number, state = 'readonly') #??? What readonly 
-number_chosen['values'] = ("Skill A","Skill B","Skill C","Skill D","Skill E","Skill F") # what does [] mean?
-number_chosen.pack(side = 'left', anchor = CENTER)#grid(column = 1, row = 4)
-number_chosen.current(0) #What does chosen.current mean?
+skillbox = ttk.Combobox (ZD, width = 12, textvariable = number, state = 'readonly') #??? What readonly 
+skillbox['values'] = ("Skill A","Skill B","Skill C","Skill D","Skill E","Skill F") # what does [] mean?
+skillbox.pack(side = 'left', anchor = tk.CENTER)#grid(column = 1, row = 4)
+skillbox.current(0) #What does chosen.current mean?
 
 
 #Adding a Button
-action = ttk.Button(ZD, text = "Search", command = print("hello Skillset")).pack(side = 'left', anchor = CENTER)#grid(column = 2, row = 4)
+def skillBoxCB():
+    print(skillbox.get())
+action = ttk.Button(ZD, text = "Search", command = skillBoxCB).pack(side = 'left', anchor = tk.CENTER)#grid(column = 2, row = 4)
 
 
 ## Adding search for Company
@@ -94,22 +97,24 @@ def click_me3():
     action.configure(text = "Search" + name.get() + '' + number_chosen.get())
     
 #Changing our Label 2
-ttk.Label(ZD, text = "Enter Your Search2:").pack(side = 'left', anchor = CENTER)#grid(column = 0, row = 5)
+#ttk.Label(ZD, text = "Enter Your Search2:").pack(side = 'left', anchor = tk.CENTER)#grid(column = 0, row = 5)
 
 # Adding a Textbox Entry widget
-name = tk.StringVar()
-name_entered = ttk.Entry(ZD, width = 12, textvariable = name).pack(side = 'left', anchor = CENTER)#grid(column = 0, row = 6)
+#name = tk.StringVar()
+#name_entered = ttk.Entry(ZD, width = 12, textvariable = name).pack(side = 'left', anchor = tk.CENTER)#grid(column = 0, row = 6)
 
 
-ttk.Label(ZD, text = "Choose an company:").pack(side = 'left', anchor = CENTER)#grid(column = 1, row = 5)
+ttk.Label(ZD, text = "Choose an company:").pack(side = 'left', anchor = tk.CENTER)#grid(column = 1, row = 5)
 number = tk.StringVar()
-number_chosen = ttk.Combobox (ZD, width = 12, textvariable = number, state = 'readonly') #??? What readonly 
-number_chosen['values'] = ("Comapny A","Company B","Comapany C","Company D","Company E","COmpany F") # what does [] mean?
-number_chosen.pack(side = 'left', anchor = CENTER)#grid(column = 1, row = 6)
-number_chosen.current(0) #What does chosen.current mean?
+companybox = ttk.Combobox (ZD, width = 12, textvariable = number, state = 'readonly') #??? What readonly 
+companybox['values'] = ("Comapny A","Company B","Comapany C","Company D","Company E","COmpany F") # what does [] mean?
+companybox.pack(side = 'left', anchor = tk.CENTER)#grid(column = 1, row = 6)
+companybox.current(0) #What does chosen.current mean?
 
 #Adding a Button
-action = ttk.Button(ZD, text = "Search", command = print("hello company")).pack(side = 'left', anchor = CENTER)#grid(column = 2, row = 6)
+def compButtonCB():
+    print(companybox.get())
+compButton = ttk.Button(ZD, text = "Search", command = compButtonCB).pack(side = 'left', anchor = tk.CENTER)#grid(column = 2, row = 6)
 
 ZD.mainloop()
 
