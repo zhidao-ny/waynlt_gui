bf10e95a5d0601fd65dd22711165867e260925f9
csuustc <48263424+csuustc@users.noreply.github.com>
2019-03-14 20:40:41 -0400

    Update querytest.py

--- querytest.py
+++ querytest.py
@@ -5,13 +5,13 @@ db = sqlite3.connect('db/testdb.sqlite')
 cursor = db.cursor()
 import pandas as pd
 def companysearch(companyname):
-    sql = "select skill, num from company_skill where comp_name = ? order by num desc limit 5"
+    sql = "select skill, num from company_skill where upper(comp_name) = upper(?) order by num desc limit 5"
     df = pd.read_sql(sql, db, params= [companyname])
     print(df)
     drawbarchart(df)
     return df
 def skillsearch(skillname):
-    sql = "select comp_name, num from company_skill where skill = ? order by num desc limit 5"
+    sql = "select comp_name, num from company_skill where upper(skill) = upper(?) order by num desc limit 5"
     df = pd.read_sql(sql, db, params= [skillname])
     print(df)
     drawbarchart(df)
